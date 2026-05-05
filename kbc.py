bc9369c74e910f64fb04978596f0e6681c1cd9cd
Priyanshurajdon <priyanshuraj8045@gmail.com>
2026-05-05 19:44:11 +0530

    KBC GAME

---
+++ kbc.py
@@ -0,0 +1,130 @@
+print("WELCOME TO KBC ")
+print("press y for yes and any other key to exit")
+x=input(str("so are you ready tobegin the game ??"))
+if (x=='y'):
+    print("welcome to game")
+else:
+    print("go and prepare")
+    exit
+
+#qusetion 1
+print("question 1:    who is jerry?")
+print( "a.mouse     b. horse     c. cat    d.kangaroo")
+a=input(str())
+if (a=="a"):
+    print("you win rupees 1000")
+else:
+    
+    print("you loose, go and prepare for it")
+    exit
+
+
+    
+#qusetion 2
+print("question 2: who are you:")
+print("a.ass    b.donkey  c.monkey  d.helicopter")
+b=input(str())
+if (b=="d"):
+    print("you win rupees 2000")
+else:
+    print("you loose, go and prepare for it")
+    exit
+
+#qusetion 3
+print("question 3.  what is you country:")
+print("a.bharat    b.china    c.pak   d.bhutan")
+c=input(str())
+if (c=="a"):
+    print("you win rupees 5000")
+else:
+    print("you loose, go and prepare for it")
+    exit
+
+#qusetion 4
+d=input(str())
+if (d=="a"):
+    print("you win rupees 1000")
+else:
+    print("you loose, go and prepare for it")
+    exit
+
+#qusetion 5
+e=input(str())
+if (e=="a"):
+    print("you win rupees 1000")
+else:
+    print("you loose, go and prepare for it")
+    exit
+
+#qusetion 6
+f=input(str())
+if (f=="a"):
+    print("you win rupees 1000")
+else:
+    print("you loose, go and prepare for it")
+    exit
+
+#qusetion 7
+g=input(str())
+if (g=="a"):
+    print("you win rupees 1000")
+else:
+    print("you loose, go and prepare for it")
+    exit
+
+#qusetion 8
+h=input(str())
+if (h=="a"):
+    print("you win rupees 1000")
+else:
+    print("you loose, go and prepare for it")
+    exit
+
+#qusetion 9
+i=input(str())
+if (i=="a"):
+    print("you win rupees 1000")
+else:
+    print("you loose, go and prepare for it")
+    exit
+
+#qusetion 10
+j=input(str())
+if (j=="a"):
+    print("you win rupees 1000")
+else:
+    print("you loose, go and prepare for it")
+    exit
+
+#qusetion 11
+k=input(str())
+if (k=="a"):
+    print("you win rupees 1000")
+else:
+    print("you loose, go and prepare for it")
+    exit
+
+#qusetion 12
+l=input(str())
+if (l=="a"):
+    print("you win rupees 1000")
+else:
+    print("you loose, go and prepare for it")
+    exit
+
+#qusetion 13
+m=input(str())
+if (m=="a"):
+    print("you win rupees 1000")
+else:
+    print("you loose, go and prepare for it")
+    exit
+
+#qusetion 14
+n=input(str())
+if (n=="a"):
+    print("you win rupees 1000")
+else:
+    print("you loose, go and prepare for it")
+    
+
